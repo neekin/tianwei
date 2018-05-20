f3acc6f9796e1041c177bfd1a03de7133e5cd520
NeeKin <njgzs360@gmail.com>
2018-05-20 11:03:39 +0800

    fix bugs

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,17 @@ import Vue from 'vue'
 Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
-        token: '',
-        username:''
+        token:  window.localStorage.token,
+        username:window.localStorage.username
     },
     mutations: {
         updateToken(state, token) {
             state.token = token
+            window.localStorage.setItem('token',token)
         },
         updateUserName(state,username){
-        	state.username = username
+            state.username = username
+            window.localStorage.setItem('username',username)
         }
     }
 })
