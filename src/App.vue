6d9c4f5745e955744d81dc55b7bdf9f13dcd833f
NeeKin <njgzs360@gmail.com>
2018-05-26 11:32:45 +0800

    hide button

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,60 @@
 <template>
-  <div id="app">
+  <div id="app" :class='{"hidesidbar":hidesidbar,"hidenav":hidenav}'>
+    	<a href="#" class="hidebtn hideleft" @click='hideleft'></a>
+			<a href="#" class="hidebtn hidetop" @click='hidetop'></a>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      hidesidbar:false,
+      hidenav:false
+    } 
+  },
+  methods:{
+     hideleft(){
+           this.hidesidbar = !this.hidesidbar;
+			},
+			hidetop(){
+				  this.hidenav =!this.hidenav;
+			}
+  }
 }
 </script>
 
 <style>
+
+.hidebtn{
+	position: fixed;
+	background-color: #004C94;
+	border-radius: 2px;
+	height: 32px;
+	width: 32px;
+	line-height: 32px;
+	margin-top:10px;
+	margin-left: 12px;
+	background-position: center center;
+	background-size: contain;
+  background-repeat: no-repeat;
+  z-index: 99;
+  top:10px;
+}
+.hideleft{
+	background-image: url(/static/right-arrow.png);
+	left:200px;
+}
+.hidetop{
+		background-image: url(/static/top-arrow.png);
+		left:240px;
+}
+.hidesidbar .hideleft{
+  left:10px;
+}
+.hidesidbar .hidetop{
+  left:50px;
+}
 </style>
